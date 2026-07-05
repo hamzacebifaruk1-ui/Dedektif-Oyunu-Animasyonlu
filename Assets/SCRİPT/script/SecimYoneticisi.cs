b2439a1e4e8e2759e2f2d64521e95d0f9c1f45ee
hamzacebifaruk1-ui <hamzacebifaruk1@gmail.com>
2026-07-05 14:57:54 +0300

    SON KISMEN

--- Assets/SCRİPT/script/SecimYoneticisi.cs
+++ Assets/SCRİPT/script/SecimYoneticisi.cs
@@ -109,7 +109,7 @@ public class SecimYoneticisi : MonoBehaviour
             }
 
             // 1 Dakika bekle ve sadece Ana Menü butonunu aç
-            yield return new WaitForSeconds(20f);
+            yield return new WaitForSeconds(10f);
             if (dogruAnaMenuButonu != null) dogruAnaMenuButonu.SetActive(true);
         }
         // 3. Yanlış Seçim Mantığı (Deli Rıza veya Kaza)
